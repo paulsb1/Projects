56f677afc19b9ce67c5d13c4b9fa0e90660811ee
paulsb <paulsb@ifi.uio.no>
2018-09-10 21:45:28 +0200

    Further improvements to Sorter

--- Sorter/README.txt
+++ Sorter/README.txt
@@ -1,16 +1,9 @@
-A simple program that takes a specified txt file containing a list of words 
-separated by whitespace or newline as input, sorts all the words 
-alphabetically while counting occurrences of each word, and writes them to
-the specified output file. A flag for whether or not the amount of occurrences
-will be printed to the output file is specified as the third argument, where
-"-y" means yes and "-n" means no (but really, anything other than "-y" will
-be recognised as no). 
-
-For testing, I used a file containg the names of all Pokemon in National Dex 
-order. The file is called "pokemon_names.txt". I also made an alternative file 
-called "pokemon_names2.txt" which contains multiple names on the same line, and
-some repetition. 
-Note: several Pokemon has names that consits of 2 words (Mr. Mime, Mime Jr., 
-Type: Null, Tapu Koko, Tapu Lele, Tapu Bulu and Tapu Fini). In order to not 
-count each of these as 2 separate words, I removed the whitespace from the 
-names.
+A simple program that takes a specified txt file that can contain e.g. a book 
+as input, sorts all the words in the file (separated by whitespace or newline)
+alphabetically while counting occurrences of each word, and writes them to the 
+specified output file. Ignores words consisting of only non-alphabetical 
+characters, and removes any non-alphabetical character, except for the hyphen, 
+from any word. A flag for whether or not the amount of occurrences will be 
+printed to the output file is specified as the third argument, where "-y" means 
+yes and "-n" means no (but really, anything other than "-y" will be recognised 
+as no). 
--- Sorter/Sorter.java
+++ Sorter/Sorter.java
@@ -6,51 +6,74 @@ import java.io.FileNotFoundException;
 
 class Sorter{
     static boolean occ_flag = false;
+    static String input_file;
+    static String output_file;
+    ArrayList<Word> sorted;
 
+    /* 
+     * Main function. Checks the arguments and opens the input file. 
+     */
     public static void main(String[] args) throws FileNotFoundException {
-    	if(args.length != 3) {
-    		System.out.println("usage: java Sorter [input file] [output file] <flag>");
-    		System.exit(0);
-    	}
-    	String input_file = args[0];
-    	String output_file = args[1];
+        if(args.length != 3) {
+            System.out.println("usage: java Sorter [input file] [output file] <flag>");
+            System.exit(0);
+        }
+        input_file = args[0];
+        output_file = args[1];
         if(args[2].equals("-y")) occ_flag = true;
 
-    	/* Trying to open the input file. */
-    	Scanner scn = null;
-    	try{
-    		scn = new Scanner(new File(input_file));
-    	} catch(FileNotFoundException e) {
-    		System.out.println("The file could not be opened.");
-    		System.exit(0);
-    	}
+        Scanner scn = null;
+        try{
+            scn = new Scanner(new File(input_file));
+        } catch(FileNotFoundException e) {
+            System.out.println("The file could not be opened.");
+            System.exit(0);
+        }
 
-        ArrayList<Word> sorted = new ArrayList<>();
+        new Sorter(scn);
+    }
+
+    Sorter(Scanner scn) {
+        read_and_sort(scn);
+        print_to_file();
+    }
+
+    /*
+     * Reads in the file, and adds all words alphabetically sorted into
+     * the arraylist. 
+     */
+    void read_and_sort(Scanner scn) {
+        sorted = new ArrayList<>();
 
         while(scn.hasNextLine()) {
             String cur_line = scn.nextLine();
-            /* Skips this iteration if the line only contains whitespace. */
+            // Skips this iteration if the line only contains whitespace.
             if(cur_line.trim().length() == 0) continue;
 
-            /* Splits the current line by whitespaces to add each individual
-            word to the sorted list. */
+            // Splits the current line by whitespaces to add each individual
+            // word to the sorted list.
             String[] line_array = cur_line.split("\\s+");
 
             for(String cur : line_array) {
+                // Skips the word if it doesn't contain any alphabetical 
+                // characters.
+                if((cur = check_word(cur)) == null) continue;
+
                 if(sorted.size() > 0) {
                     int j = 0;
-                    int cmp = 1; //cur.compareTo(sorted.get(j).get_content());
+                    int cmp = 1; // Arbitrary value, has to be > 0. 
 
-                    /* Compares to the sorted list and finds the right 
-                    position to place the word. */
+                    // Compares to the sorted list and finds the right 
+                    // position to place the word.
                     while(cmp > 0 && j < sorted.size()) {
-                        cmp = cur.compareTo(sorted.get(j).get_content());
+                        String cmp_to = sorted.get(j).get_content().toLowerCase();
+                        cmp = compare(cur.toLowerCase(), cmp_to);
                         if(cmp <= 0) break;
                         j++;
                     }
 
-                    /* Add the word to the list if it's a new word, 
-                    increase occurrences of the word if not. */
+                    // Add the word to the list if it's a new word, 
+                    // increase occurrences of the word if not.
                     if(cmp != 0) {
                         Word w = new Word(cur);
                         sorted.add(j, w);
@@ -58,15 +81,36 @@ class Sorter{
                         sorted.get(j).increase_occurrences();
                     }
                 } else { 
-                    /* If it's the first word, just add it. */
+                    // If it's the first word, just add it.
                     Word w = new Word(cur);
                     sorted.add(w);
                 }
             }
         }
+    }
 
-    	/* Writing the sorted list to the output file. */
-    	PrintWriter pw = new PrintWriter(output_file);
+    /* 
+     * Removes all non-alphabetical characters from a word except the
+     * hyphen. Returns null if word consisted only of numbers and/or 
+     * punctuation marks or symbols. 
+     */
+    String check_word(String word) {
+        word = word.replaceAll("[^A-Za-z\\-]", "");
+        if(word.length() == 0) return null;
+        if(word.matches("[\\-]+")) return null; // Special case for only hyphens.
+        return word;
+    }
+
+    /*
+     * Writes the sorted list to the output file.
+     */ 
+    void print_to_file() {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(output_file);
+        } catch(FileNotFoundException e) {
+            System.exit(0);
+        }
 
         if(occ_flag) {
             for(int i = 0; i < sorted.size(); i++) {
@@ -79,8 +123,8 @@ class Sorter{
                 if(i != sorted.size()-1) pw.print("\n");
             }
         }
-    	
-    	pw.close();
-    	System.out.println("Done.");
+        
+        pw.close();
+        System.out.println("Done.");
     }
 }
